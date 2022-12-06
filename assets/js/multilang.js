b9eaa10365a86f0d63b2ffab39a912628dd629f1
Alessio Alfieri <supermediatablet@gmail.com>
2022-12-06 13:29:34 +0100

    leng done

--- assets/js/multilang.js
+++ assets/js/multilang.js
@@ -5,9 +5,90 @@ document.getElementById("it").addEventListener("click", () => {
   document.getElementById("contact-navbar").innerHTML =
     '<i class="icon-speech"></i>CONTATTACI';
   document.getElementById("title-hero").innerHTML =
-    "Agenzia di crescita <br> Influencer";
+    "Onlyfans <br> Creator Management";
   document.getElementById("start-hero").innerHTML =
     '<i class="icon-cup"></i>INIZIAMO';
+  document.getElementById("why-us").innerHTML = "Perchè sceglierci?";
+  document.getElementById("why-us-desc").innerHTML =
+    "Curiamo e gestiamo la crescita di ogni creator con attenzione e seguendo le specifiche più adatte per ogni caso in questione.";
+  document.getElementById("partner").innerHTML = "Incremento Collaborazioni";
+  document.getElementById("follow").innerHTML = "Incremento Follower";
+  document.getElementById("problems").innerHTML = "Meno Problemi";
+  document.getElementById("gallery").innerHTML = "Galleria";
+  document.getElementById("servizi").innerHTML = "Servizi";
+  document.getElementById("shooting").innerHTML = "Shooting fotografico";
+  document.getElementById("shooting-desc").innerHTML =
+    "Ti aiutiamo a creare i migliori contenuti digitali grazie a shooting professionali e personalizzati a seconda del cliente";
+  document.getElementById("chat").innerHTML = "Gestione chat";
+  document.getElementById("chat-desc").innerHTML =
+    "I nostri collaboratori gestiranno le chat su ogni social 24/7 per massimizzare i profitti ed avere una più comoda gestione dei messaggi";
+  document.getElementById("social").innerHTML = "Promo social";
+  document.getElementById("social-desc").innerHTML =
+    "Aiutiamo la crescita sui social tramite collaborazioni con pagine o profili in target";
+  document.getElementById("marketing").innerHTML = "Marketing";
+  document.getElementById("marketing-desc").innerHTML =
+    "Creazione di un identità e di un personaggio dedita alla crescita social";
+  document.getElementById("collab").innerHTML = "Collab";
+  document.getElementById("collab-desc").innerHTML =
+    "Aiutiamo i nostri clienti creando un vero e proprio network dove riuscire a distinguersi e crescere";
+  document.getElementById("personal").innerHTML = "Servizi personalizzati";
+  document.getElementById("personal-desc").innerHTML =
+    "A seconda di richieste specifiche creiamo servizi su misura per ogni esigenza";
+  document.getElementById("manag").innerHTML = "Gestione delle chat";
+  document.getElementById("manag2").innerHTML = "Gestione delle chat";
+  document.getElementById("manag3").innerHTML = "Gestione delle chat";
+
+  document.getElementById("piani").innerHTML = "Piani";
+  document.getElementById("adv").innerHTML = "Consulenza personalizzata";
+  document.getElementById("adv2").innerHTML = "Consulenza personalizzata";
+  document.getElementById("adv3").innerHTML = "Consulenza personalizzata";
+  document.getElementById("allinc").innerHTML =
+    "Gestione dei contenuti completa";
+  document.getElementById("allinc2").innerHTML =
+    "Gestione dei contenuti completa";
+  document.getElementById("24").innerHTML = "Consulenza 24/7";
+  document.getElementById("242").innerHTML = "Consulenza 24/7";
+  document.getElementById("foto").innerHTML = "Shooting fotografico";
+  document.getElementById("pers").innerHTML = "Servizi personalizzati";
+
+  document.getElementById("choose1").innerHTML =
+    '<i class="icon-arrow-right-circle" ></i>SCEGLI PIANO';
+  document.getElementById("choose2").innerHTML =
+    '<i class="icon-arrow-right-circle" ></i>SCEGLI PIANO';
+  document.getElementById("choose3").innerHTML =
+    '<i class="icon-arrow-right-circle" ></i>SCEGLI PIANO';
+
+  document.getElementById("success").innerHTML = "Il Nostro Successo";
+  document.getElementById("succ-desc").innerHTML =
+    "Lavoriamo accanto a ogni creator da ormai 5 anni.";
+  document.getElementById("succ-desc2").innerHTML =
+    "Garantiamo un eccellenza nel settore avendo aiutato molti influencers a uscire dall’ignoto o supportando la loro crescita.";
+  document.getElementById("quote").innerHTML =
+    "Portiamo il tuo lavoro ad un altro livello";
+  document.getElementById("contattaci").innerHTML = "CONTATTACI";
+
+  document.getElementById("name").setAttribute("placeholder", "Nome");
+  document.getElementById("email").setAttribute("placeholder", "Email");
+  document.getElementById("phone").setAttribute("placeholder", "Telefono");
+  document.getElementById("message").setAttribute("placeholder", "Messaggio");
+
+  document.getElementById("option1").innerHTML =
+    "Qual'è il servizio che ti interessa di più?";
+  document.getElementById("option2").innerHTML = "Servizi Personalizzati";
+  document.getElementById("option4").innerHTML = "Gestione dei contenuti";
+  document.getElementById("option6").innerHTML = "Gestione delle chat";
+
+  document.getElementById("submit").innerHTML = "INVIA";
+
+  document.getElementById("gettt").innerHTML = "Contattaci";
+  document.getElementById("serv").innerHTML = "I Nostri Servizi";
+  document.getElementById("m").innerHTML =
+    '<i class="icon-speech"></i>INVIA UN MESSAGGIO';
+
+  document.getElementById("foo1").innerHTML = "Shooting fotografico";
+  document.getElementById("foo2").innerHTML = "Gestione delle chat";
+  document.getElementById("foo3").innerHTML = "Social promo";
+  document.getElementById("foo4").innerHTML = "Servizi personalizzati";
 });
 
 document.getElementById("en").addEventListener("click", () => {
@@ -16,7 +97,90 @@ document.getElementById("en").addEventListener("click", () => {
   document.getElementById("plans-navbar").innerHTML = "Plans";
   document.getElementById("contact-navbar").innerHTML =
     '<i class="icon-speech"></i>CONTACT US';
-  document.getElementById("title-hero").innerHTML = "Influencer<br>Grow Agency";
+  document.getElementById("title-hero").innerHTML =
+    "Onlyfans <br> Creator Management";
   document.getElementById("start-hero").innerHTML =
     '<i class="icon-cup"></i>GET STARTED';
+  document.getElementById("why-us").innerHTML = "Why choose us?";
+  document.getElementById("why-us-desc").innerHTML =
+    "We see all types of projects as if they were ours. This brings us closer to our clients' projects bringing much more confidence and commitment.";
+  document.getElementById("partner").innerHTML = "Partnership Increment";
+  document.getElementById("follow").innerHTML = "Follower Increment";
+  document.getElementById("problems").innerHTML = "Less problems";
+  document.getElementById("gallery").innerHTML = "Gallery";
+  document.getElementById("servizi").innerHTML = "Servicies";
+  document.getElementById("shooting").innerHTML = "Photo shooting";
+  document.getElementById("shooting-desc").innerHTML =
+    "High quality photo services to enhance your profile";
+  document.getElementById("chat").innerHTML = "Chat management";
+  document.getElementById("chat-desc").innerHTML =
+    "Professional chat management on any social to maximize revenue";
+  document.getElementById("social").innerHTML = "Social promo";
+  document.getElementById("social-desc").innerHTML =
+    "Organic and exponential growth of social profiles with promos in target with customer profile";
+  document.getElementById("marketing").innerHTML = "Marketing";
+  document.getElementById("marketing-desc").innerHTML =
+    'Image management combined with the development or creation of "character" identity';
+  document.getElementById("collab").innerHTML = "Collab";
+  document.getElementById("collab-desc").innerHTML =
+    "We help you make connections and increase visibility through on-target partnerships";
+  document.getElementById("personal").innerHTML = "Personalized services";
+  document.getElementById("personal-desc").innerHTML =
+    "Creating customized services according to customer needs";
+
+  document.getElementById("piani").innerHTML = "Plans";
+  document.getElementById("manag").innerHTML = "Chat management";
+  document.getElementById("manag2").innerHTML = "Chat management";
+  document.getElementById("manag3").innerHTML = "Chat management";
+
+  document.getElementById("adv").innerHTML = "Personalized advices";
+  document.getElementById("adv2").innerHTML = "Personalized advices";
+  document.getElementById("adv3").innerHTML = "Personalized advices";
+  document.getElementById("allinc").innerHTML =
+    "All-inclusive content management";
+  document.getElementById("allinc2").innerHTML =
+    "All-inclusive content management";
+  document.getElementById("24").innerHTML = "24/7 advices";
+  document.getElementById("242").innerHTML = "24/7 advices";
+  document.getElementById("foto").innerHTML = "Photo shooting";
+  document.getElementById("pers").innerHTML = "Personalized services";
+
+  document.getElementById("choose1").innerHTML =
+    '<i class="icon-arrow-right-circle" ></i>CHOSE PLAN';
+  document.getElementById("choose2").innerHTML =
+    '<i class="icon-arrow-right-circle" ></i>CHOSE PLAN';
+  document.getElementById("choose3").innerHTML =
+    '<i class="icon-arrow-right-circle" ></i>CHOSE PLAN';
+
+  document.getElementById("success").innerHTML = "Success Story";
+  document.getElementById("succ-desc").innerHTML =
+    "We have been working with influencers and models for 5 years.";
+  document.getElementById("succ-desc2").innerHTML =
+    "Our work is recognized by all our customers. We guarantee an excellent quality service in every way.";
+  document.getElementById("quote").innerHTML =
+    "We take your work to the next level";
+  document.getElementById("contattaci").innerHTML = "GET IN TOUCH";
+
+  document.getElementById("name").setAttribute("placeholder", "Name");
+  document.getElementById("email").setAttribute("placeholder", "Email");
+  document.getElementById("phone").setAttribute("placeholder", "Phone");
+  document.getElementById("message").setAttribute("placeholder", "Message");
+
+  document.getElementById("option1").innerHTML =
+    "Which service are you most interested in?";
+  document.getElementById("option2").innerHTML = "Personalized service";
+  document.getElementById("option4").innerHTML = "Content management";
+  document.getElementById("option6").innerHTML = "Chat management";
+
+  document.getElementById("submit").innerHTML = "SEND";
+
+  document.getElementById("gettt").innerHTML = "Get in Touch";
+  document.getElementById("serv").innerHTML = "Our Services";
+  document.getElementById("m").innerHTML =
+    '<i class="icon-speech"></i>SEND A MESSAGE';
+
+  document.getElementById("foo1").innerHTML = "Photo shooting";
+  document.getElementById("foo2").innerHTML = "Chat management";
+  document.getElementById("foo3").innerHTML = "Social promo";
+  document.getElementById("foo4").innerHTML = "Personalized services";
 });
